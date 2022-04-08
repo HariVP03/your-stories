c7bd2e07f769eaa578f6b743979eabab2c0d22e1
HariVP03 <harryskotch11@gmail.com>
2022-04-08 14:28:14 +0530

    fix

--- src/services/graphql/index.ts
+++ src/services/graphql/index.ts
@@ -8,7 +8,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject> | null = null;
 
 const createApolloClient = new ApolloClient({
     ssrMode: typeof window === "undefined",
-    uri: "https://api.spacex.land/graphql/",
+    uri: "https://your-stories-123.herokuapp.com/",
     cache: new InMemoryCache(),
 });
 
