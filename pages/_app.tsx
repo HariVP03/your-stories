12b0ffde7eae4867badc7f82a2ff20ca496c5665
HariVP03 <harryskotch11@gmail.com>
2022-04-13 00:48:34 +0530

    changed metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,14 +9,55 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 import { RecoilRoot } from "recoil";
 import Head from "next/head";
-import { SEO } from "@components";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     const apolloClient = initializeApollo();
     const queryClient = new QueryClient();
     return (
         <>
-            <SEO />
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <meta
+                    name="title"
+                    content="Your Stories - Write and share your stories with the world"
+                />
+                <meta
+                    name="description"
+                    content="Write and share your stories and thoughts with the world with ease and read what other people have to share."
+                />
+
+                {/* <!-- Open Graph / Facebook --> */}
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:url"
+                    content="https://your-stories-hari.netlify.app/"
+                />
+                <meta
+                    property="og:title"
+                    content="Your Stories - Write and share your stories with the world"
+                />
+                <meta
+                    property="og:description"
+                    content="Write and share your stories and thoughts with the world with ease and read what other people have to share."
+                />
+                <meta property="og:image" content="/your-stories.png" />
+
+                {/* <!-- Twitter --> */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta
+                    property="twitter:url"
+                    content="https://your-stories-hari.netlify.app/"
+                />
+                <meta
+                    property="twitter:title"
+                    content="Your Stories - Write and share your stories with the world"
+                />
+                <meta
+                    property="twitter:description"
+                    content="Write and share your stories and thoughts with the world with ease and read what other people have to share."
+                />
+                <meta property="twitter:image" content="/your-stories.png" />
+            </Head>
             <ChakraProvider theme={theme}>
                 <ApolloProvider client={apolloClient}>
                     <QueryClientProvider client={queryClient}>
