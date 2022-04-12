4fc7ead752fa7718c0fd6e81b496965ab90f0bc2
HariVP03 <harryskotch11@gmail.com>
2022-04-13 00:08:44 +0530

    added metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,46 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <>
             <Head>
                 <link rel="shortcut icon" href="/favicon.ico" />
+                <meta
+                    name="title"
+                    content="Your Stories - Write and share your stories with the world"
+                />
+                <meta
+                    name="description"
+                    content="Write and share your stories and thoughts with the world with ease and read what other people have to share."
+                />
+
+                {/* <!-- Open Graph / Facebook --> */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://metatags.io/" />
+                <meta
+                    property="og:title"
+                    content="Your Stories - Write and share your stories with the world"
+                />
+                <meta
+                    property="og:description"
+                    content="Write and share your stories and thoughts with the world with ease and read what other people have to share."
+                />
+                <meta
+                    property="og:image"
+                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+                />
+
+                {/* <!-- Twitter --> */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://metatags.io/" />
+                <meta
+                    property="twitter:title"
+                    content="Your Stories - Write and share your stories with the world"
+                />
+                <meta
+                    property="twitter:description"
+                    content="Write and share your stories and thoughts with the world with ease and read what other people have to share."
+                />
+                <meta
+                    property="twitter:image"
+                    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+                />
             </Head>
             <ChakraProvider theme={theme}>
                 <ApolloProvider client={apolloClient}>
